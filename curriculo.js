d1718b7cbee79cd35d25d8faffa7715a9cff1a9c
Mateus Rigon <mateusrigon5@gmail.com>
2020-03-28 18:12:30 -0300

    mudanças no visual e funçôes

--- curriculo.js
+++ curriculo.js
@@ -2,6 +2,18 @@ function jumpScroll() {
 	window.scroll(0, 0);
 }
 
-function jumpScrollab() {
+function jumpScrolla() {
    	window.scroll(0, 630);
+}
+
+function jumpScrollb() {
+   	window.scroll(0, 1270);
+}
+
+function jumpScrollc() {
+   	window.scroll(0, 1895);
+}
+
+function jumpScrolld() {
+   	window.scroll(0, 2300);
 }
